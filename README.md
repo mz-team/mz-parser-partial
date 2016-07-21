4bb3eda75d9e1f8ae79df48ebe717d86df0519c7
yekai <yekai@meizu.com>
2016-07-21 19:38:11 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,31 @@
+# mz-parser-partial
+## 1、使用方法
+
+
+```javascript
+fis.match('*.tpl', {
+    parser: [
+      fis.plugin('partial',{type:'ssi'}),
+      function (content, file, settings) {
+        return content.replace(/\{\{(?:(\w+)\:)?([\.\w]+)\}\}/g, function(str, func, arg ){
+          if(func){
+            return fis[func](arg);
+          }else{
+            return fis.get(arg);
+          }
+        });
+    }]
+});
+```
+
+## 2、实现原理
+
+默认输出
+```
+<{include file="{{namespace}}/page/_partial/header.tpl"}>
+```
+
+配置为 ssi 后输出
+```
+<!--#include virtual="{{urlprefix}}/_partial/header.html"-->
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Parser 阶段插件接口
+ * @param  {string} content     文件内容
+ * @param  {File}   file        fis 的 File 对象 [fis3/lib/file.js]
+ * @param  {object} settings    插件配置属性
+ * @return {string}             处理后的文件内容
+ */
+module.exports = function (content, file, settings) {
+  var type = settings.type;
+  return content.replace(/\{\{(?:(\w+)\:)?([\.\w]+)\}\}/g, function (str, func, arg) {
+    if (func !== 'partial') return str;
+
+    if (type === 'ssi') {
+      return '<!--#include virtual="{{urlprefix}}/_partial/' + arg + '.html"-->';
+    } else {
+      return '<{include file="{{namespace}}/page/_partial/' + arg + '.tpl"}>';
+    }
+  });
+}
